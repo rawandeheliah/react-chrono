a20845efb0f61dfad4a30e3ef20f7a779f3bd5f3
rawandeheliah <rawandeheliah@gmail.com>
2022-01-03 11:43:14 +0200

    add dist

---
+++ dist/components/timeline-horizontal/timeline-horizontal-cricle.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import { HorizontalCircleModel } from '../../models/TimelineHorizontalModel';
+declare const HorizontalCircle: React.FunctionComponent<HorizontalCircleModel>;
+export default HorizontalCircle;
